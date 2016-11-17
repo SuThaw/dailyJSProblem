eb9168f493310a1e90f1bf9150d728e2c1323186
SuThaw <suthaw524@gmail.com>
2016-11-17 15:59:25 +0630

    added two string problem

--- test.js
+++ test.js
@@ -6,9 +6,13 @@ const assert = require('assert');
 
 /*----------  Testing For String Repeat Library  ----------*/
 
-const repeatString = require('./repeat-string');
+const StringProblem = require('./string-problem');
 
-assert.deepEqual(repeatString(2,'abc'),'abcabc');
+assert.deepEqual(StringProblem.repeatStr(2,'abc'),'abcabc');
+
+assert.deepEqual(StringProblem.longest('defff','aaabc'),'abcdef');
+
+console.log('Passed all the test')
 
 
 
